e3e1ac99f9e73a9da21bdc618272f2bc1f97f7af
Crazy code <67009087+cuiqianna@users.noreply.github.com>
2025-06-04 16:08:55 +0800

    Add files via upload

---
+++ MM_pca.py
@@ -0,0 +1,239 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as io
+from spectral import *
+from skimage.morphology import disk
+from skimage.morphology import reconstruction
+from skimage.morphology import erosion
+from skimage.morphology import dilation
+import os
+import cv2
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import time
+
+
+def superpixel_size_compute(index, count):
+
+    count_num = len(count)
+
+    #ob_diag = np.zeros(shape=(count_num), dtype=np.uint32)
+    short_dis = np.zeros(shape=(count_num), dtype=np.uint32)
+
+    for i in range(count_num):
+        x_set = np.where(index == i)[0]
+        y_set = np.where(index == i)[1]
+
+        x_min = np.min(x_set)
+        x_max = np.max(x_set)
+        y_min = np.min(y_set)
+        y_max = np.max(y_set)
+
+        x_dis = x_max - x_min +1
+        y_dis = y_max - y_min +1
+
+        sh = min([x_dis, y_dis])
+        short_dis[i] = sh
+
+    sh_min = min(short_dis)
+
+    return sh_min
+
+
+
+def opening_by_reconstruction(image, se):
+    image_e = erosion(image, se)
+    obr = reconstruction(image_e, image, method='dilation')
+
+    return (obr)
+
+
+def closing_by_reconstruction(image, se):
+    # fechamento por reconstrução
+    image_d = dilation(image, se)
+    cbr = reconstruction(image_d, image, method='erosion')
+
+    return (cbr)
+
+
+def close_pca(image, initial_size, se_size_increment, max_size):
+
+    tam = initial_size
+
+    img_r = image[:, :, 0]
+    img_g = image[:, :, 1]
+    img_b = image[:, :, 2]
+
+    size_sum = int((max_size - initial_size) / se_size_increment) + 1
+    size_sum *= 3
+
+    height = image.shape[0]
+    width = image.shape[1]
+
+    close_matrix = np.zeros(shape=(height, width, size_sum), dtype=np.float32)
+
+    while tam <= max_size:
+        se = disk(tam)
+        cur_r = int((tam - initial_size) / se_size_increment)
+        cur_g = int((tam - initial_size) / se_size_increment) + 1
+        cur_b = int((tam - initial_size) / se_size_increment) + 2
+
+        close_r = closing_by_reconstruction(img_r, se)
+        close_g = closing_by_reconstruction(img_g, se)
+        close_b = closing_by_reconstruction(img_b, se)
+
+        close_matrix[:, :, cur_r] = close_r.copy()
+        close_matrix[:, :, cur_g] = close_g.copy()
+        close_matrix[:, :, cur_b] = close_b.copy()
+        tam += se_size_increment
+
+        pca_result = PCA_reduce(close_matrix)
+        return pca_result
+
+
+
+
+
+def open_pca(image, initial_size, se_size_increment, max_size):
+    img_r = image[:,:,0]
+    img_g = image[:,:,1]
+    img_b = image[:,:,2]
+
+    tam = initial_size
+
+    size_sum = int((max_size - initial_size) / se_size_increment) + 1
+    size_sum *= 3
+
+
+    height = img_r.shape[0]
+    width = img_r.shape[1]
+
+    open_matrix = np.zeros(shape= (height, width, size_sum), dtype= np.uint32)
+
+
+    while tam <= max_size:
+        se = disk(tam)
+        cur_r = int((tam- initial_size) / se_size_increment)
+        cur_g = int((tam- initial_size) / se_size_increment) + 1
+        cur_b = int((tam- initial_size) / se_size_increment) + 2
+
+        open_r = opening_by_reconstruction(img_r,se)
+
+        open_g = opening_by_reconstruction(img_g, se)
+
+        open_b = opening_by_reconstruction(img_b, se)
+
+        open_matrix[:,:, cur_r] = open_r.copy()
+        open_matrix[:,:, cur_g] = open_g.copy()
+        open_matrix[:,:, cur_b] = open_b.copy()
+
+        tam += se_size_increment
+    pca_result = PCA_reduce(open_matrix)
+    return pca_result
+
+def PCA_reduce(matrix_old):
+    height = matrix_old.shape[0]
+    width = matrix_old.shape[1]
+    channel = matrix_old.shape[2]
+    num_sum = height * width
+
+    matrix = np.zeros(shape=(num_sum, channel), dtype=np.uint32)
+    for h in range(height):
+        for w in range(width):
+            id = h * width + w
+            for i in range(channel):
+                matrix[id, i] = matrix_old[h, w, i]
+
+    sc = StandardScaler()
+    pixels = sc.fit_transform(matrix)
+
+    pca = PCA(n_components=1)
+    pc = pca.fit_transform(pixels)
+    # print(pc.shape)
+
+    matrix_new = np.zeros(shape=(height, width), dtype=np.float32)
+    for h in range(height):
+        for w in range(width):
+            id = h * width + w
+            matrix_new[h, w] = pc[id]
+
+    return matrix_new
+
+
+def im_show(image, pca_matrix):
+    height = image.shape[0]
+    width = image.shape[1]
+
+    image_new = image.copy()
+
+    for h in range(height):
+        for w in range(width):
+            open_pca_val = pca_matrix[h, w]
+            if (open_pca_val <= 0):
+                image_new[h, w, :] = 0
+    return image_new
+
+def MM_pca_processing(image, initial_size, size_increment, max_size):
+    start = time.time()
+    open_pca_matrix = open_pca(image, initial_size, size_increment, max_size)
+    open_image = im_show(image, open_pca_matrix)
+
+    close_pca_matrix = close_pca(open_image, initial_size, size_increment, max_size)
+    end = time.time()
+
+    MM_t = end - start
+    #return open_pca_matrix, MM_t
+    return close_pca_matrix, MM_t
+
+
+
+
+
+#
+# if __name__== '__main__':
+#
+#     main_folder = r"./"
+#     file_str = ["ArcGIS", 'WDMI', 'google']
+#     seg_str_set = ['SLIC', 'LSC', 'SNIC', 'FSLIC', 'CMSuG']
+#
+#     time_matrix = np.zeros(shape=(3,5), dtype= np.float32)
+#
+#     for i in range(1,2):
+#         cur_str = file_str[i]
+#
+#         file_folder = os.path.join(main_folder, cur_str)
+#
+#         image_folder = os.path.join(file_folder, "GMM")
+#
+#         image_save_folder = os.path.join(file_folder, "MM")
+#
+#         image_file_name = os.path.join(image_folder, "GMM_LAB.jpg")
+#         image = plt.imread(image_file_name)
+#
+#         initial_size = 3
+#         size_increment = 2
+#         #max_size = superpixel_size_compute(index, count)
+#         max_size = 3
+#         tem = '%d' % max_size
+#
+#         #print(cur_str, ":", str_seg, ":", "max_size--", max_size)
+#
+#         #start = time.time()
+#
+#         open_pca_matrix = open_pca(image, initial_size, size_increment, max_size)
+#         cur_open_file = os.path.join(image_save_folder, tem + "_open_pca.jpg")
+#         cur_open_csv = os.path.join(image_save_folder, tem + "_open_pca.csv")
+#         plt.imsave(cur_open_file, open_pca_matrix, cmap='gray')
+#         np.savetxt(cur_open_csv, open_pca_matrix, delimiter= ',', fmt = '%.4f')
+#
+#         open_image = im_show(image, open_pca_matrix)
+#
+#         close_pca_matrix = close_pca(open_image, initial_size, size_increment, max_size)
+#         #
+#         cur_close_jpg = os.path.join(image_save_folder, tem + "_close_pca.jpg")
+#         plt.imsave(cur_close_jpg, close_pca_matrix, cmap='gray')
+#
+#         cur_close_csv = os.path.join(image_save_folder, tem + "_close_pca.csv")
+#         np.savetxt(cur_close_csv, close_pca_matrix, delimiter=',', fmt='%.4f')
+#
+#
---
+++ efficient_graph_merge.py
@@ -0,0 +1,553 @@
+import numpy as np
+import math
+import os
+from matplotlib import  pyplot as plt
+import time
+#
+import matplotlib as mpl
+mpl.use('Agg')
+
+
+def read_component(csv_file):
+    with open(csv_file, mode='r', encoding= 'utf-8') as f:
+        all = f.readlines()
+
+    component =[]
+    for i in all:
+        i = i.strip().split(',')
+        temp =[]
+        for j in i:
+            temp.append(int(j))
+        component.append(temp)
+    return component
+
+def read_tau(csv_file):
+    with open(csv_file, mode='r', encoding= 'utf-8') as f:
+        all = f.readlines()
+
+    component =[]
+    for i in all:
+        i = i.strip().split(',')
+        temp =[]
+        for j in i:
+            temp.append(float(j))
+        component.append(temp)
+    tau = []
+    num = len(component)
+    for i in range(num):
+        x = (component[i])[0]
+        tau.append(x)
+    return tau
+
+
+
+def class_component(old_com):
+    num = len(old_com)
+    one_com = []
+#    short_com =[]
+    long_com = []
+    for i in range(num):
+        cur_com = old_com[i]
+        cur_com_num = len(cur_com)
+        if (cur_com_num == 1):
+            one_com.append(i)
+        else:
+            # if (cur_com_num <= 10):
+            #     short_com.append(i)
+            # else:
+            long_com.append(i)
+    return one_com, long_com
+
+
+def object_cal(object_diff, count, adj_idx):
+
+    object = []
+    num = len(count)
+    for i in range(1, num):
+        cur_count = count[i]
+        object_adj = []
+        adj_diff = []
+        for j in range(1, num):
+            if adj_idx[i,j] == 1:
+                object_adj.append(j)
+                diff_val = object_diff[i,j]
+                adj_diff.append(diff_val)
+
+        object.append((i,object_adj, adj_diff, cur_count))
+    return object
+
+def com_adj_cal(component, object):
+    cur_ob = []
+    for x in component:
+        ob = object[x-1]
+        cur_ob.append(ob)
+    return cur_ob
+
+def component_index(component, index):
+    xx = []
+    yy = []
+    for co in component:
+        x_set = np.where(index == co)[0]
+        y_set = np.where(index == co)[1]
+        xn = len(x_set)
+        for j in range(xn):
+            dx = x_set[j]
+            dy = y_set[j]
+            xx.append(dx)
+            yy.append(dy)
+    x_min = min(xx)
+    x_max = max(xx) +1
+    y_min = min(yy)
+    y_max = max(yy) +1
+    new_index = index[x_min:x_max, y_min: y_max]
+
+    return x_min, x_max, y_min, y_max, new_index
+
+def reseg_idx(seg_result, index, image):
+    num = len(seg_result)
+    height = index.shape[0]
+    width = index.shape[1]
+    new_index = np.zeros(shape= (height, width), dtype= np.uint32)
+    new_image = image.copy()
+    for i in range(num):
+        new_id = 1 + i
+        ri = seg_result[i]
+        r_num = len(ri)
+        for j in range(r_num):
+            id = ri[j]
+            x_set = np.where(index == id)[0]
+            y_set = np.where(index == id)[1]
+
+            xn = len(x_set)
+            for k in range(xn):
+                dx = x_set[k]
+                dy = y_set[k]
+
+                new_index[dx, dy] = new_id
+    for h in range(height):
+        for w in range(width):
+            if (new_index[h,w] == 0):
+                new_image[h,w, :] = 0
+    return new_index, new_image
+
+def compute_seg_count(seg_index):
+    #计算当前融合后的总segments
+    seg_id = []
+    height = seg_index.shape[0]
+    width = seg_index.shape[1]
+    for h in range(height):
+        for w in range(width):
+            id = seg_index[h,w]
+
+            if (id not in seg_id):
+                seg_id.append(id)
+    if (0 in seg_id):
+        num = len(seg_id) -1
+    else:
+        num = len(seg_id)
+    return num
+
+
+def var_cal(image, com_index):
+    hh = com_index.shape[0]
+    ww = com_index.shape[1]
+
+    id_max = np.max(com_index) + 1
+
+    image_new = np.zeros(shape=(hh, ww, 3), dtype=np.uint8)
+
+    color = np.zeros(shape=(id_max, 3), dtype=np.float32)
+    mean = np.zeros(shape=(id_max, 3), dtype=np.float32)
+
+    var_sum = np.zeros(shape=(id_max, 3), dtype=np.float32)
+    var = np.zeros(shape=(id_max, 3), dtype=np.float32)
+
+    for i in range(1, id_max):
+        x_set = np.where(com_index == i)[0]
+        y_set = np.where(com_index == i)[1]
+        xn = len(x_set)
+
+        for j in range(xn):
+            dx = x_set[j]
+            dy = y_set[j]
+
+            color[i, 0] += image[dx, dy, 0]
+            color[i, 1] += image[dx, dy, 1]
+            color[i, 2] += image[dx, dy, 2]
+        mean[i, 0] = color[i, 0] / xn
+        mean[i, 1] = color[i, 1] / xn
+        mean[i, 2] = color[i, 2] / xn
+
+        for j in range(xn):
+            dx = x_set[j]
+            dy = y_set[j]
+
+            dif_r = image[dx,dy,0] - mean[i,0]
+            dif_g = image[dx, dy, 1] - mean[i, 1]
+            dif_b = image[dx, dy, 2] - mean[i, 2]
+
+            var_sum[i,0] += pow(dif_r, 2)
+            var_sum[i,1] += pow(dif_g,2)
+            var_sum[i,2] += pow(dif_b,2)
+        var[i, 0] = var_sum[i,0] / xn
+        var[i, 1] = var_sum[i, 1] / xn
+        var[i, 2] = var_sum[i, 2] / xn
+
+    r_sum = 0.
+    g_sum =0.
+    b_sum = 0.
+    for i in range(1, id_max):
+        r_sum += var[i,0]
+        g_sum += var[i,1]
+        b_sum += var[i,2]
+
+    var_val = (r_sum + g_sum + b_sum) / 3
+
+    return var_val
+
+
+
+class graph_merging:
+    def __init__(self, object,k):
+        #self.time = tt
+        self.k = k
+        self.object = object
+        self.result = []
+        self.graph_processing()
+
+    def graph_processing(self):
+        self.Int = []
+        adj_index = []
+        diff_v = []
+        num = len(self.object)
+        self.object_index = []
+        for i in range(num):
+
+            id = (self.object[i])[0]
+            self.object_index.append(id)
+            adj_idx = (self.object[i])[1]
+
+            adj_num = len(adj_idx)
+
+            dif = (self.object[i])[2]
+            cur_ob_num = (self.object[i])[3]
+
+            for j in range(adj_num):
+                adj_index.append([id,adj_idx[j]])
+                diff_v.append(dif[j])
+
+
+        a = np.sort(diff_v)
+        b = np.argsort(diff_v)
+        c = []
+        for i in range(b.shape[0]):
+            c.append(adj_index[b[i]])
+
+        self.result.append(c[0])
+
+
+        for i in range(1,b.shape[0]):
+            r_num = len(self.result)
+            for j in range(r_num):
+                r_i = self.result[j]
+                r_len = len(r_i)
+                Int = []
+                for r in r_i:
+                    #id = (self.object[r])[0]
+                    id = self.object_index.index(r)
+                    id_adj = (self.object[id])[1]
+                    cur_diff = (self.object[id])[2]
+                    for rj in r_i:
+                        if (rj in id_adj and rj != r):
+                            r_a = id_adj.index(rj)
+
+                            Int.append(cur_diff[r_a])
+                if (Int == []):
+                    Int2 = 0
+                else:
+                    Int2 = self.MInt(Int, r_len)
+                self.Int.append(Int2)
+            x_a = a[i]
+            x_b = c[i]
+
+            x1 = -1
+            x2 = -1
+            for j in range(r_num):
+                r_i = self.result[j]
+                r_len = len(r_i)
+                for k in range(r_len):
+                    if (r_i[k] == x_b[0]):
+                        x1 = j
+
+            for k in range(r_num):
+                r_i = self.result[k]
+                r_len = len(r_i)
+                for k_2 in range(r_len):
+                    if (r_i[k_2] == x_b[1]):
+                        x2 = k
+
+            if (x1 == -1 and x2 == -1):
+                self.result.append(x_b)
+
+            if (x1 != -1 and x2 == -1):
+                x_s1 = self.Int[x1]
+                if x_a <= x_s1:
+                    self.result[x1].append(x_b[1])
+                else:
+                    self.result.append([x_b[1]])
+            if (x1 == -1 and x2 != -1):
+                    x_s2 = self.Int[x2]
+                    if x_a <= x_s2:
+                        self.result[x2].append(x_b[0])
+                    else:
+                        self.result.append([x_b[0]])
+
+            if (x1 != -1 and x2 != -1):
+                if (x1 == x2):
+                    self.result = self.result
+                elif (x1 != x2):
+                    x_s1 = self.Int[x1]
+                    x_s2 = self.Int[x2]
+                    x_min = min(x_s1,x_s2)
+                    #print(x_min)
+
+                    if (x_a <= x_min):
+                        r2_l = len(self.result[x2])
+                        for rr in range(r2_l):
+                            self.result[x1].append((self.result[x2])[rr])
+                        self.result.remove(self.result[x2])
+                    else:
+                        self.result =self.result
+
+            self.result = [x for x in self.result if x != []]
+
+            #print(len(self.result))
+            #print(len(self.Int))
+            self.Int = []
+        return self.result
+
+    def MInt(self,Int, n):
+        t = self.k / n
+        b = [i + t for i in Int]
+        x = max(b)
+
+        return x
+
+
+def efficient_graph_processing(image, index, component, adj_matrix, count, object_diff, save_folder):
+    img_new = image.copy()
+    height = image.shape[0]
+    width = image.shape[1]
+
+    for h in range(height):
+        for w in range(width):
+            if (index[h, w] == 0):
+                img_new[h, w, :] = 0
+
+    object_adj = object_cal(object_diff, count, adj_matrix)
+
+    com_read = component.copy()
+    one_com, long_com = class_component(com_read)#保存的是com在component中的索引
+    merge_com_num = len(long_com)
+
+    time_sum = 0.
+    optimal_tau_set = [] # save all the tau_value of all object_index
+
+    for k in range(merge_com_num):
+        ii = long_com[k]  #所有component中需要融合的com索引
+        cur_com = com_read[ii]
+        #print(k)
+        #print(cur_com)
+
+        tem_com = '%d' % (ii +1)
+
+        seg_num = []
+
+        tau_set =[]
+        tau = 0.02
+        seg_num_sum = len(seg_num)
+
+        tau_opti = tau
+
+        cur_object = com_adj_cal(cur_com, object_adj)
+        x_min, x_max, y_min, y_max, cur_index = component_index(cur_com, index)
+        cur_im = img_new[x_min: x_max, y_min: y_max]
+
+        cur_index_opt = cur_index.copy()
+        opti_var = 99999999.
+
+        while ((seg_num_sum < 15) and (1 not in seg_num)):
+
+            tem_tau = '%.2f' % tau
+
+            start_1 = time.time()
+            seg_result = graph_merging(cur_object, tau)
+            end_1 = time.time()
+
+            t_1 = end_1- start_1
+            time_sum += t_1
+
+            result = seg_result.result
+            cur_new_index, new_image = reseg_idx(result, cur_index, cur_im)
+            cur_seg_num = compute_seg_count(cur_new_index)
+
+            if (cur_seg_num not in seg_num):
+                seg_num.append(cur_seg_num)
+                seg_num_sum += 1
+                cur_height = cur_index.shape[0]
+                cur_width = cur_index.shape[1]
+
+                if (cur_height == 1 or cur_width == 1):
+                    cur_index_opt = cur_new_index.copy()
+                else:
+                    start_2 = time.time()
+                    cur_var= var_cal(cur_im, cur_new_index)
+                    if (cur_var < opti_var):
+                        tau_opti = tau
+                        cur_index_opt = cur_new_index.copy()
+                        opti_var = cur_var
+                    else:
+                        pass
+                    end_2 = time.time()
+                    t_2 = end_2 - start_2
+                    time_sum += t_2
+            else:
+                pass
+            tau += 0.02
+        optimal_tau_set.append(tau_opti)
+
+        save_cur_index_name = os.path.join(save_folder,  str(tem_com) +  ".csv")
+        np.savetxt(save_cur_index_name, cur_index_opt, delimiter=',', fmt='%d')
+    save_tau_opt_set_name = os.path.join(save_folder, "tau.csv")
+    np.savetxt(save_tau_opt_set_name, optimal_tau_set, delimiter=',', fmt = '%.2f')
+    return time_sum
+
+
+
+#
+# if __name__== '__main__':
+#
+#     main_folder = r"./"
+#     file_str = ["ArcGIS", 'WDMI', 'google']
+#     seg_str_set = ['SLIC', 'LSC', 'SNIC', 'FSLIC', 'CMSuG']
+#
+#     for i in range(1,2):
+#         cur_str = file_str[i]
+#         print(cur_str)
+#         file_folder = os.path.join(main_folder, cur_str)
+#         image_file_name = os.path.join(file_folder, cur_str + ".jpg")
+#         image = plt.imread(image_file_name)
+#         height = image.shape[0]
+#         width = image.shape[1]
+#
+#         index_folder = os.path.join(file_folder, "pre_seg/TC/2000")
+#         component_folder = os.path.join(file_folder, "pre_seg/TC/2000")
+#         save_folder = os.path.join(file_folder, "pre_seg/TC/2000")
+#
+#
+#         for j in range(1):
+#             str_seg = seg_str_set[j]
+#             #print(str_seg)
+#
+#             cur_number = 2000
+#             tem_number = '%d' % cur_number
+#
+#             #subgraph_folder = os.path.join(component_folder, str_seg)
+#
+#             index_file = os.path.join(index_folder, str(tem_number) +  "_new.csv")
+#             index = np.loadtxt(open(index_file), delimiter=',', dtype=np.uint32, skiprows=0)
+#             #print(np.max(index))
+#
+#             count_file = os.path.join(index_folder, str(tem_number) +  "_count.csv")
+#             count = np.loadtxt(open(count_file), delimiter=',', dtype=np.uint32, skiprows=0)
+#             #print(len(count))
+#
+#             adj_file = os.path.join(index_folder, str(tem_number) + "_adj_new.csv")
+#             adj_matrix = np.loadtxt(open(adj_file), delimiter= ',', dtype= np.uint8, skiprows= 0)
+#
+#             object_diff_file = os.path.join(index_folder, str(tem_number) +  "_object_diff.csv")
+#             object_diff = np.loadtxt(open(object_diff_file), delimiter=',', dtype= np.float32, skiprows= 0)
+#
+#             object_adj = object_cal(object_diff, count, adj_matrix)
+#             #print(len(count))
+#             #print(object_adj)
+#
+#
+#
+#             img_new = image.copy()
+#             for h in range(height):
+#                 for w in range(width):
+#                     if (index[h, w] == 0):
+#                         img_new[h, w, :] = 0
+#
+#             component_save_file = os.path.join(component_folder, str(tem_number) +  "_component.csv")
+#             com_read = read_component(component_save_file)
+#
+#             one_com, long_com = class_component(com_read)#保存的是com在component中的索引
+#             merge_com_num = len(long_com)
+#
+#             time_sum = 0
+#
+#             for k in range(merge_com_num):
+#                 ii = long_com[k]  #所有component中需要融合的com索引
+#                 cur_com = com_read[ii]
+#                 #print(k)
+#                 #print(cur_com)
+#
+#                 tem_com = '%d' % (ii +1)
+#
+#                 seg_num = []
+#
+#                 tau_set =[]
+#                 tau = 0.02
+#                 seg_num_sum = len(seg_num)
+#
+#
+#                 while ((seg_num_sum < 15) and (1 not in seg_num)):
+#                     cur_object = com_adj_cal(cur_com, object_adj)
+#                     x_min, x_max, y_min, y_max, cur_index = component_index(cur_com, index)
+#                     new_im = img_new[x_min: x_max, y_min: y_max]
+#
+#                     tem_tau = '%.2f' % tau
+#
+#                     start = time.time()
+#                     seg_result = graph_merging(cur_object, tau)
+#                     end = time.time()
+#
+#                     t = end- start
+#                     time_sum += t
+#
+#                     result = seg_result.result
+#                     new_index, new_image = reseg_idx(result, cur_index, new_im)
+#                     cur_seg_num = compute_seg_count(new_index)
+#                     if (cur_seg_num not in seg_num):
+#                         seg_num.append(cur_seg_num)
+#                         tau_set.append(tau)
+#                         seg_num_sum += 1
+#
+#                         new_index_name = os.path.join(save_folder,  str(tem_com) +"_" + str(tem_tau) + ".csv")
+#                         np.savetxt(new_index_name, new_index, delimiter=',', fmt='%d')
+#
+#                         result_im = new_image.copy()
+#                         im_height = result_im.shape[0]
+#                         im_width = result_im.shape[1]
+#                         for h in range(1, im_height - 1):
+#                             for w in range(1, im_width - 1):
+#                                 if (new_index[h, w] != new_index[h - 1, w] or new_index[h, w] != new_index[h + 1, w] or
+#                                         new_index[h, w] != new_index[h, w - 1] or new_index[h, w] != new_index[
+#                                             h, w + 1]):
+#                                     result_im[h, w, 0] = 255
+#                                     result_im[h, w, 1] = 0
+#                                     result_im[h, w, 2] = 0
+#
+#                         #new_img_name = os.path.join(subgraph_folder, str_seg + "_" + str(tem_com) +"_" + str(tem_tau) + ".jpg")
+#                         #plt.imsave(new_img_name, result_im)
+#                     else:
+#                         pass
+#                     tau += 0.02
+#                     tem_tau = '%.2f' % tau
+#                 tau_name = os.path.join(save_folder,  str(tem_com) + "_" + "tau.csv")
+#                 np.savetxt(tau_name, tau_set, delimiter=',', fmt='%.2f')
+#             print(cur_str, ",", index_file, ",", "time-", time_sum)
+#
+#
